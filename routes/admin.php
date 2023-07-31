7f8afc7985619c870484f866fa83f524c011e500
Selim Şahin <selimsahin.sns@gmail.com>
2023-07-31 20:19:38 +0300

    created slider model, controller and basic create slider page

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,17 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
+/** Admin Routes */
 Route::redirect('/', '/admin/dashboard', 302);
 Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
+/** Profile Routes */
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::post('/profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+/** Slider Route */
+Route::resource('slider', SliderController::class);
