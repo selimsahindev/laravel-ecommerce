62743c2c6ccb20cbc73827599b94b2a097f53fdf
Selim Şahin <selimsahin.sns@gmail.com>
2023-08-06 01:11:11 +0300

    added a custom helpers.php to composer.json, dynamically displaying the admin sidebar items as active

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -8,27 +8,32 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown active">
+            <li class="dropdown {{ setActive(['admin.dashboard']) }}">
                 <a href="{{ route('admin.dashboard') }}" class="nav-link"><i
                         class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Starter</li>
 
-            <li class="dropdown">
+            <li
+                class="dropdown {{ setActive(['admin.category.*', 'admin.sub-category.*', 'admin.child-category.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Manage Categories</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.category.index') }}">Categories</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.sub-category.index') }}">Sub Categories</a></li>
-                    <li><a class="nav-link" href="{{ route('admin.child-category.index') }}">Child Categories</a></li>
+                    <li class="{{ setActive(['admin.category.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.category.index') }}">Categories</a></li>
+                    <li class="{{ setActive(['admin.sub-category.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.sub-category.index') }}">Sub Categories</a></li>
+                    <li class="{{ setActive(['admin.child-category.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.child-category.index') }}">Child Categories</a></li>
                 </ul>
             </li>
 
-            <li class="dropdown">
+            <li class="dropdown {{ setActive(['admin.slider.*']) }}">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Manage Website</span></a>
                 <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="{{ route('admin.slider.index') }}">Sliders</a></li>
+                    <li class="{{ setActive(['admin.slider.*']) }}"><a class="nav-link"
+                            href="{{ route('admin.slider.index') }}">Sliders</a></li>
                 </ul>
             </li>
 
